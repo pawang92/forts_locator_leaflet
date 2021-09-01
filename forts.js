a2661fbac5813569e171c93135fdcd75fb4ee2fb
LAPTOP-E1J81TC6\pawan <pawang92@rediffmail.com>
2021-09-01 16:16:45 +0530

    first commit for forts

---
+++ forts.js
@@ -0,0 +1,304 @@
+const fortList = [
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [82.87574688225702, 25.123370135669393]
+        },
+        "properties": {
+            "name": "Chunar Fort",
+            "rating": "Rating : 4.1",
+            "address": "Tammanpatti, Uttar Pradesh 231304",
+            "phone": "9554599675"
+
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [83.02485966004592, 25.27029711599199]
+        },
+        "properties": {
+            "name": "Ramnagar Fort",
+            "rating": "Rating : 4.1",
+            "address": "Varanasi Rd, Purana, Ramnagar, Uttar Pradesh 221008",
+            "phone": "9554599675"
+        }
+
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [80.4852614883307, 24.99735946191833]
+        },
+        "properties": {
+            "name": "Kalinjar Fort",
+            "rating": "Rating : 4.4",
+            "address": "Kalinjar, Madhya Pradesh 210129",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [81.87684551225531, 25.42954190700266]
+        },
+        "properties": {
+            "name": "Prayagraj Fort",
+            "rating": "Rating : 4.4",
+            "address": " Allahabad fort, Prayagraj, Uttar Pradesh 211005",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [78.02110663556977, 27.179541151134035]
+        },
+        "properties": {
+            "name": "Agra Fort",
+            "rating": "Rating : 4.5",
+            "address": "Agra Fort, Rakabganj, Agra, Uttar Pradesh 28200",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [82.68850975274823, 25.749621819835756]
+        },
+        "properties": {
+            "name": "Jaunpur Fort",
+            "rating": "Rating : 4.1",
+            "address": "Rizwikhan, Jaunpur, Uttar Pradesh 222001",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [72.81558233445242, 19.330550678560023]
+        },
+        "properties": {
+            "name": "Vasai Fort",
+            "rating": "Rating : 4.3",
+            "address": "Killa Rd, Police Colony, Vasai West, Vasai-Virar, Maharashtra 401201",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [72.8183506828088, 19.041887298420534]
+        },
+        "properties": {
+            "name": "Bandra Fort",
+            "rating": "Rating : 4.4",
+            "address": "Byramji Jeejeebhoy Road, Bandstand Road, Mumbai, Maharashtra 400050",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [72.86445441132948, 18.634206510286386]
+        },
+        "properties": {
+            "name": "Kulaba Fort",
+            "rating": "Rating : 4.2",
+            "address": "Alibag, Maharashtra 402201",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [73.44643058893547, 18.23523153286786]
+        },
+        "properties": {
+            "name": "Raigad Fort",
+            "rating": "Rating : 4.7",
+            "address": "Raigad, Maharashtra 402305",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [75.8513471288284, 26.985491490237933]
+        },
+        "properties": {
+            "name": "Amber Palace",
+            "rating": "Rating : 4.6",
+            "address": "Devisinghpura, Amer, Jaipur, Rajasthan 302001",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [78.40105134903057, 17.383314400818094]
+        },
+        "properties": {
+            "name": "Golconda Fort",
+            "rating": "Rating : 4.4",
+            "address": "Khair Complex, Ibrahim Bagh, Hyderabad, Telangana 500008",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [75.84475927053948, 26.981603373205704]
+        },
+        "properties": {
+            "name": "Jaigarh Fort",
+            "rating": "Rating : 4.5",
+            "address": "Devisinghpura, Amer, Jaipur, Rajasthan 302028",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [73.31799284722635, 28.02196984689351]
+        },
+        "properties": {
+            "name": "Junagarh Fort",
+            "rating": "Rating : 4.5",
+            "address": "Bikaner Fort, Bikaner, Rajasthan 334001",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [73.01884679998729, 26.29795420690963]
+        },
+        "properties": {
+            "name": "Mehrangarh Museum and Trust",
+            "rating": "Rating : 4.6",
+            "address": "P.B # 165, The, Fort Rd, Jodhpur, Rajasthan 342006",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [73.58699829998727, 25.152826327874696]
+        },
+        "properties": {
+            "name": "Kumbhalgarh Fort",
+            "rating": "Rating : 4.3",
+            "address": "Kumbhalgarh, Rajasthan 313325",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [76.28619349876182, 27.096452359305136]
+        },
+        "properties": {
+            "name": "Bhangarh Fort",
+            "rating": "Rating : 4.3",
+            "address": "Tehsil, Gola ka baas, Rajgarh, Bhangarh, Rajasthan 301410",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [77.24099601229727, 28.65609879607074]
+        },
+        "properties": {
+            "name": "Red Fort",
+            "rating": "Rating : 4.4",
+            "address": "Netaji Subhash Marg, Lal Qila, Chandni Chowk, New Delhi, Delhi 110006",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [74.64508991777853, 24.887900316560017]
+        },
+        "properties": {
+            "name": "Chittorgarh Fort",
+            "rating": "Rating : 4.6",
+            "address": "Chittor Fort Road, Chittorgarh, Rajasthan 312001",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [79.12947302520924, 12.921091939426478]
+        },
+        "properties": {
+            "name": "Vellore Fort",
+            "rating": "Rating : 4.3",
+            "address": "Balaji Nagar, Vellore, Tamil Nadu 632004",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [77.56482705889331, 8.125855400000004]
+        },
+        "properties": {
+            "name": "Vattakottai Fort",
+            "rating": "Rating : 4.3",
+            "address": "Vattakkottai, Tamil Nadu 629401",
+            "phone": "9554599675"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [78.16949570501596, 26.231326969316175]
+        },
+        "properties": {
+            "name": "Gwalior Fort",
+            "rating": "Rating : 4.5",
+            "address": "Gwalior, Madhya Pradesh 474008",
+            "phone": "07512234557"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [88.335871556126, 22.55424676222501]
+        },
+        "properties": {
+            "name": "William Fort",
+            "rating": "Rating : 4.5",
+            "address": "Maidan, Fort William, Hastings, Kolkata, West Bengal 700021",
+            "phone": "9554599675"
+        }
+    },
+
+]
